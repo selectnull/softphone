cd2ccd5e6a0d5a8a7466a2ac6bf6c029d061516c
Sasha Matijasic <sasha@selectnull.com>
2021-07-01 22:07:26 +0200

    Make setup toggleable

--- frontend/setup.js
+++ frontend/setup.js
@@ -1,17 +1,22 @@
 class Setup {
-  static fields = ['apiUsername', 'apiPassword', 'webrtcUsername', 'webrtcPassword', 'phoneNumber'];
+  static fields() {
+    return Array.from(document.querySelectorAll('#setup .content input')).map(el => el.id);
+  }
 
   static load() {
-    Setup.fields.forEach(field => {
+    Setup.fields().forEach(field => {
       document.querySelector(`#${field}`).value = localStorage.getItem(field);
     });
   }
 
   static save() {
-    Setup.fields.forEach(field => {
+    Setup.fields().forEach(field => {
       localStorage.setItem(field, document.querySelector(`#${field}`).value);
     });
+  }
 
+  static toggle() {
+    $('#setup .content').toggle();
   }
 }
 
