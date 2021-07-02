0ea1fd1ee37309bf0daecfe34387ed906ceffb1d
Sasha Matijasic <sasha@selectnull.com>
2021-07-02 11:01:28 +0200

    Receive a call.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import httpx
 
+from pathlib import Path
+import logging
+
 from .config import get_var
 
+LOG_FILE = Path.home() / 'logs' / 'phone.logit.net.log'
+logging.basicConfig(
+    filename=LOG_FILE,
+    encoding='utf-8', level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s'
+)
 
 app = FastAPI()
+
 
 class VoiceStart(BaseModel):
     connect: str
@@ -37,6 +47,21 @@ async def forward(call: Call):
     async with httpx.AsyncClient() as client:
         response = await client.post('https://api.46elks.com/a1/calls', auth=auth, data=data)
     return response.json()
+
+
+@app.post('/api/receive-call/{connect_to_phone}')
+async def receive_call(
+    connect_to_phone: int,
+    direction: str = Form(...),
+    callid: str=Form(...),
+    from_number: str=Form(..., alias='from'),
+    to_number: str=Form(..., alias='to')
+):
+    log_msg = f'Call from {from_number} to +{connect_to_phone}'
+    logging.info(log_msg)
+    return {
+        'connect': f'+{connect_to_phone}'
+    }
 
 
 if get_var('call_serve_frontend') == 'yes':
